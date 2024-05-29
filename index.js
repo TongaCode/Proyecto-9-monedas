4e97df00139c527db5549d7e2606dce0530445ab
Gaston Iaria <iariagaston@gmail.com>
2024-05-29 05:28:26 -0300

    Agrege un endpoint para el segundo pesaje

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const { balanza } = require("./balanza");
 const { elejir } = require("./elejir");
-const {comoJugar} = require("./comoJugar")
+const { comoJugar } = require("./comoJugar")
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post("/jugar", (req, res) => {
+app.post("/primerPesaje", (req, res) => {
   balanza(req, res);
+});
+
+app.post("/segundoPesaje", (req, res) => {
+
 });
 
 app.post("/elejir", (req, res) => {
