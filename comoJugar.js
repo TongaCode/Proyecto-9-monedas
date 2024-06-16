12775765fe94d40bc58a8ff52ccb4c9fa5e22aa3
Gaston Iaria <iariagaston@gmail.com>
2024-06-16 01:18:21 -0300

    Modifique agregue el texto al modulo pra poder exportarlo

--- comoJugar.js
+++ comoJugar.js
@@ -1,3 +1,3 @@
-const comoJugar = "Tienes 9 monedas en total que son iguales pero solo una es mas pesada que las otras, con una balanza tendras que poder sacar que moneda es mas pesada, pero ojo solo tienes dos oportunidades osea solo puedes pesar dos veces, una vez que termines puedes adivinar que moneda es la mas pesada, en jugar podras poner las monedas en la balanza, en elejir podras poner la moneda que te parece mas pesada para saber si ganaste o perdiste, suerte!!!";
+const texto = "Tienes 9 monedas en total que son iguales pero solo una es mas pesada que las otras, con una balanza tendras que poder sacar que moneda es mas pesada, pero ojo solo tienes dos oportunidades osea solo puedes pesar dos veces, una vez que termines puedes adivinar que moneda es la mas pesada, en jugar podras poner las monedas en la balanza, en elejir podras poner la moneda que te parece mas pesada para saber si ganaste o perdiste, suerte!!!";
 
-module.exports = {comoJugar}
+module.exports = { texto } 
