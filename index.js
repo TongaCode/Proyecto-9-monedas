7813e6781dd63a9d091c7503a8b5f3f98c6d4ec2
Gaston Iaria <iariagaston@gmail.com>
2024-06-16 01:30:31 -0300

    Modifique y agregue los endpoint de Primer y segundo pesaje

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require("express");
-const { balanza } = require("./balanza");
+const { balanzaPrimerPesaje } = require("./primerPesaje");
+const { balanzaSegundoPesaje } = require("./segundoPesaje");
 const { elejir } = require("./elejir");
-const { comoJugar } = require("./comoJugar")
+const { texto } = require("./comoJugar");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+app.get("/comoJugar", (req, res) => {
+  res.send(texto)
+});
+
 app.post("/primerPesaje", (req, res) => {
-  balanza(req, res);
+  balanzaPrimerPesaje(req, res);
 });
 
 app.post("/segundoPesaje", (req, res) => {
-
+  balanzaSegundoPesaje(req, res);
 });
 
 app.post("/elejir", (req, res) => {
   elejir(req, res);
 });
 
-app.get("/comoJugar", (req, res) => {
-  res.send(comoJugar)
-});
 
 app.listen(port, () => {
   console.log(`Server online en el puerto ${port}`);
